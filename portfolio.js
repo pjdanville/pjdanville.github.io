996c7b95be77e3e93d1f7f7a98f06944efba0ce1
PJ <79735815+pjdanville@users.noreply.github.com>
2021-07-20 03:21:36 -0400

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+alert ("Check out my resume!");
+
+ const menu = document.querySelector('#mobileMenu')
+ const menuLinks = document.querySelector('.navMenu')
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active');
+});
